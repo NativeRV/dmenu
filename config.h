408fae8563ce8be84b273880ae0e875fd5b6f806
NativeRV <nativerv@protonmail.com>
2021-09-06 07:41:10 +0300

    feat: revert `dmenu-center-4.8`; add prompt to `dmenu_run`

--- config.h
+++ config.h
@@ -5,16 +5,19 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FiraCode Nerd Font:size=12"
+	"monospace:size=11"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
+// static const char *colors[SchemeLast][2] = {
+	// [>     fg         bg       <]
+	// [SchemeNorm] = { "#bbbbbb", "#222222" },
+	// [SchemeSel] = { "#eeeeee", "#005577" },
+	// [SchemeOut] = { "#000000", "#00ffff" },
+// };
+// [> -l option; if nonzero, dmenu uses vertical list with given number of lines <]
+
+#include "/home/nrv/.cache/wal/colors-wal-dmenu.h"
+
 static unsigned int lines      = 15;
 
 /*
@@ -24,4 +27,4 @@ static unsigned int lines      = 15;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 2;
+static const unsigned int border_width = 0;
