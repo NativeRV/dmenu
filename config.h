3632d7ceabe6b9ef8946510202e77e241ad0cf3c
NativeRV <nativerv@protonmail.com>
2021-09-08 16:50:27 +0300

    feat(patch): add `dmenu-xresources-alt-5.0`

--- config.h
+++ config.h
@@ -4,20 +4,25 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static char font[] = "monospace:size=11";
 static const char *fonts[] = {
-	"monospace:size=11"
+  font,
+  "JoyPixels:pixelsize=10:antialias=true:autohint=true",
 };
 static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
-// static const char *colors[SchemeLast][2] = {
-	// [>     fg         bg       <]
-	// [SchemeNorm] = { "#bbbbbb", "#222222" },
-	// [SchemeSel] = { "#eeeeee", "#005577" },
-	// [SchemeOut] = { "#000000", "#00ffff" },
-// };
 // [> -l option; if nonzero, dmenu uses vertical list with given number of lines <]
 
-#include "/home/nrv/.cache/wal/colors-wal-dmenu.h"
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
 
+static const char *colors[SchemeLast][2] = {
+	/*               fg           bg       */
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor  },
+	[SchemeOut]  = { "#000000",   "#00ffff"   },
+};
 static unsigned int lines      = 15;
 
 /*
@@ -25,6 +30,18 @@ static unsigned int lines      = 15;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "font",        STRING, &font },
+	{ "foreground",  STRING, &normfgcolor },
+	{ "background",  STRING, &normbgcolor },
+	{ "selfgcolor",  STRING, &selfgcolor },
+	{ "color1",      STRING, &selbgcolor },
+	{ "color2",      STRING, &prompt },
+};
 
 /* Size of the window border */
 static const unsigned int border_width = 0;
