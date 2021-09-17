a91342c47fe5b2c9eeea79e0877452f27afbc6af
NativeRV <nativerv@protonmail.com>
2021-09-17 17:16:03 +0300

    feat(patch): add `dmenu-lineheight-5.0` and integrate in to `dmenu-xresources-alt-5.0`

--- config.h
+++ config.h
@@ -1,16 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;
+
+/* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int fuzzy = 1;
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[] = "monospace:size=11";
+static char font[] = "monospace:size=9";
 static const char *fonts[] = {
   font,
-  "JoyPixels:pixelsize=10:antialias=true:autohint=true",
+  "JoyPixels:pixelsize=9:antialias=true:autohint=true",
 };
-static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
-// [> -l option; if nonzero, dmenu uses vertical list with given number of lines <]
+
+/* -p  option; prompt to the left of input field */
+static const char *prompt = NULL;
+
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines          = 0;
+
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight     = 0;
+static unsigned int min_lineheight = 8;
 
 static char normfgcolor[] = "#bbbbbb";
 static char normbgcolor[] = "#222222";
@@ -23,7 +35,6 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel]  = { selfgcolor,  selbgcolor  },
 	[SchemeOut]  = { "#000000",   "#00ffff"   },
 };
-static unsigned int lines      = 15;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -35,12 +46,15 @@ static const char worddelimiters[] = " ";
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-	{ "font",        STRING, &font },
-	{ "foreground",  STRING, &normfgcolor },
-	{ "background",  STRING, &normbgcolor },
-	{ "selfgcolor",  STRING, &selfgcolor },
-	{ "color1",      STRING, &selbgcolor },
-	{ "color2",      STRING, &prompt },
+	{ "font",            STRING,     &font },
+	{ "foreground",      STRING,     &normfgcolor },
+	{ "background",      STRING,     &normbgcolor },
+	{ "selfgcolor",      STRING,     &selfgcolor },
+	{ "color1",          STRING,     &selbgcolor },
+	{ "color2",          STRING,     &prompt },
+  { "lineheight",      INTEGER,    &lineheight }, 
+  { "min_lineheight",  INTEGER,    &lineheight }, 
+  { "lines",           INTEGER,    &lines }, 
 };
 
 /* Size of the window border */
